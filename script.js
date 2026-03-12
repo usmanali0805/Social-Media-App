aaaae4d23233b03b5518be770aa2bca587b442c7
usmanali0805 <usman.ali08052005@gmail.com>
2026-03-12 23:27:02 +0500

    Added SignUp functionality through supabase

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-const { createClient } = supabase;
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
 
-// Create a single supabase client for interacting with your database
+
 const SUPABASE_URL = 'https://sfkmmfyntkupmcgxccfg.supabase.co'
 const API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNma21tZnludGt1cG1jZ3hjY2ZnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE5NTM5OTIsImV4cCI6MjA4NzUyOTk5Mn0.YFwoq6_N7WHAJSYzRlD_WHJzzttHSIRRVCpHN0ikzM4'
-const supabaseClient = createClient(SUPABASE_URL, API_KEY)
+const supabase = createClient(SUPABASE_URL, API_KEY);
+
+
 
 //  Protect Feed Page
 if (window.location.pathname.includes("index.html")) {
@@ -15,73 +17,106 @@ if (window.location.pathname.includes("index.html")) {
     }
 }
 
-const signupbtn= document.getElementById('Signupbtn')
+const signupbtn = document.getElementById('Signupbtn')
 const loginbtn = document.getElementById('loginBtn')
 const logoutbtn = document.getElementById('logout')
-const postbtn  = document.getElementById('createPost')
+const postbtn = document.getElementById('createPost')
 
 
-if(signupbtn){
+if (signupbtn) {
     signupbtn.addEventListener("click", signup)
 }
 
-if(loginbtn){
-    loginbtn.addEventListener("click",login)
+if (loginbtn) {
+    loginbtn.addEventListener("click", login)
 }
 
-if(logoutbtn){
-logoutbtn.addEventListener("click",logout)
+if (logoutbtn) {
+    logoutbtn.addEventListener("click", logout)
 }
 
 if (postbtn) {
-    postbtn.addEventListener("click" , createPost)
+    postbtn.addEventListener("click", createPost)
 }
 
 
 // Signup
-function signup(){
+async function signup() {
     const firstname = document.getElementById("signupFirstName").value;
     const lastname = document.getElementById("signupLastName").value;
     const email = document.getElementById("signupEmail").value;
     const password = document.getElementById("signupPassword").value;
 
-    const user = { firstname,lastname, email, password };
-    localStorage.setItem("user", JSON.stringify(user));
+    if (!firstname || !lastname || !email || !password) {
+        alert("Please fill all requirment");
+        return;
+    }
 
-    alert("Signup successful!");
-    window.location.href = "login.html";
-}
 
-// Login
-function login(){
-    const email = document.getElementById("loginEmail").value;
-    const password = document.getElementById("loginPassword").value;
+    // const user = { firstname, lastname, email, password };
+    // localStorage.setItem("user", JSON.stringify(user));
+    const { data, error } = await supabase.auth.signUp({
+        email,
+        password,
+         options: {
+            data: {
+                firstname: firstname,
+                lastname: lastname
+            }
+        }
+    })
 
-    const savedUser = JSON.parse(localStorage.getItem("user"));
-
-    if(savedUser && savedUser.email === email && savedUser.password === password){
-        localStorage.setItem("currentUser", JSON.stringify(savedUser));
-        window.location.href = "index.html";
+    if (data) {
+        console.log(data)
+        // alert("Signup successful!");
+        // window.location.href = "login.html";
     } else {
-        alert("Invalid credentials");
+        console.log(error, '===> error')
     }
 }
 
+
+
+// Login
+async function login() {
+    const email = document.getElementById("loginEmail").value;
+    const password = document.getElementById("loginPassword").value;
+
+    const { data, error } = await supabase.auth.signInWithOtp({
+        email,
+    })
+    if (data) {
+        alert("Login succussfully")
+    }
+    else {
+        console.log(error, 'errror')
+    }
+
+    // const savedUser = JSON.parse(localStorage.getItem("user"));
+
+    // if (savedUser && savedUser.email === email && savedUser.password === password) {
+    //     localStorage.setItem("currentUser", JSON.stringify(savedUser));
+    //     window.location.href = "index.html";
+    // } else {
+    //     alert("Invalid credentials");
+    // }
+}
+
 // Logout
-function logout(){
+function logout() {
     localStorage.removeItem("currentUser");
     window.location.href = "login.html";
 }
 
 // Create Post
-function createPost(){
+function createPost() {
     const text = document.getElementById("postText").value;
     const imageUrl = document.getElementById("imageUrl").value;
 
-    if(text.trim() === "") return;
+    if (text.trim() === "") return;
 
     const currentUser = JSON.parse(localStorage.getItem("currentUser"));
-     
+
     console.log(currentUser)
     const post = {
         text,
@@ -102,7 +137,7 @@ function createPost(){
 }
 
 // Load Posts
-function loadPosts(){
+function loadPosts() {
     const postsContainer = document.getElementById("posts");
     postsContainer.innerHTML = "";
 
